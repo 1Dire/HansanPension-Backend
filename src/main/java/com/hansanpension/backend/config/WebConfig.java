a308f2c5da1c7a871e1f23868e2d08205e9141cf
1Dire <d01038145300@nate.com>
2025-05-14 18:50:47 +0900

    cors 수정

--- src/main/java/com/hansanpension/backend/config/WebConfig.java
+++ src/main/java/com/hansanpension/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 URL 패턴에 대해
-                .allowedOrigins("http://localhost:5173")  // Vite 리액트 앱 주소
+                .allowedOrigins("http://localhost:5173", "http://61.75.57.247:5173")  // 여러 origin을 쉼표로 구분
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 쿠키나 인증 정보를 요청에 포함할 수 있도록 설정
